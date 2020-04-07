4a56ff292a93b4a7ab2b90725a38e5d6d06f369c
Agith Yogendra <agith.yogendra@gmail.com>
2020-04-07 17:26:45 -0400

    Switch to spaCy

--- word_embedding_graph.py
+++ word_embedding_graph.py
@@ -1,6 +1,3 @@
-#from gensim.models import Word2Vec
-import gensim.models.keyedvectors as Word2Vec
-from gensim.similarities import WmdSimilarity
 import os
 from nltk.corpus import stopwords
 import nltk
@@ -15,50 +12,61 @@ import networkx as nx
 import matplotlib
 from networkx.readwrite import json_graph
 import json
+from bs4 import BeautifulSoup
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+import spacy
+from spacy.lang.en import English
 
+start = time()
+
+nltk.download('wordnet')
 nltk.download("punkt")   # Download data for tokenizer.
 stop_words = stopwords.words('english')
+
+porter = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
 
 G = nx.Graph()
 doc_corpus = []
 w2v_corpus = []
-path = 'clueweb09PoolFilesTest'  
+path = 'clueweb09_50'  
 
 def preprocess(doc):
     doc = doc.lower()  # Lower the text.
     doc = word_tokenize(doc)  # Split into words.
     doc = [w for w in doc if not w in stop_words]  # Remove stopwords.
-    doc = [w for w in doc if w.isalpha()]  # Remove numbers and punctuation.
-    return doc
-i = 0
+    doc = [w for w in doc if w.isalpha()]  # Remove numbers and punctuation
+    return ' '.join(doc)
+    
 #Get file names
 for file in os.listdir(path):
-    if i == 50:
-        break
     f = open(path + '/' + file)
     doc_corpus.append(file)
     # Pre-process document.
-    tmp = f.read() 
-    text = preprocess(tmp)
+    html = f.read() 
+    soup = BeautifulSoup(html, "html.parser")
+    text = soup.get_text()
+    text = porter.stem(text)
+    text= lemmatizer.lemmatize(text)
+    text = preprocess(text)
     # Add to corpus for training Word2Vec.
     w2v_corpus.append(text)
     f.close()
-    i = i + 1
 
-if not os.path.exists('GoogleNews-vectors-negative300.bin'):
-    raise ValueError("SKIP: You need to download the google news model")
-start = time()
-model = Word2Vec.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)    
-model.init_sims(replace=True)
-model.save('word_embeddings')
+# train model
+nlp = spacy.load('en_core_web_md')
 
 for i in range(len(doc_corpus)):
     for j in range(len(doc_corpus)):
-        if i == j:
+        if i == j or G.has_edge(doc_corpus[i], doc_corpus[j]):
             continue
-        G.add_edge(doc_corpus[i], doc_corpus[j], weight = Word2Vec.KeyedVectors.load('word_embeddings', mmap = 'r').wmdistance(w2v_corpus[i], w2v_corpus[j]))
-
+        # Vectorize docs
+        doc1 = nlp(w2v_corpus[i])
+        doc2 = nlp(w2v_corpus[j])
+        sim = doc1.similarity(doc2)
+        G.add_edge(doc_corpus[i], doc_corpus[j], weight = sim)
 graph_json = json_graph.node_link_data(G)
-json.dump(graph_json, open('graphGood.json', 'w'), indent = 2)
+json.dump(graph_json, open('clueweb09_50.json', 'w'), indent = 2)
 print ('Cell took %.2f seconds to run.' %(time() - start))
 
