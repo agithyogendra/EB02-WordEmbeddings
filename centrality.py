6436222310c19a72a6d78a45ecd0980b0a5a5aff
Agith Yogendra <agith.yogendra@gmail.com>
2020-04-29 21:24:23 -0400

    Add Multithreading

--- centrality.py
+++ centrality.py
@@ -1,8 +1,11 @@
 import networkx as nx
 from networkx.readwrite import json_graph
 import json
-from decimal import Decimal
+from multiprocessing.dummy import Pool as ThreadPool
+from time import time
 
+sdm_results = 'results.txt'
+start_project = time()
 # Get graph from json file 
 def getGraph(filename):
   with open(filename) as f:
@@ -23,8 +26,15 @@ def get_threshold(G):
       weight_sum += weight
   return weight_sum/count
 
+graph = getGraph('clueweb09PoolFilesTest.json')
+threshold = get_threshold(graph)
+scores = {}
 # Return top 20 ranked documents from subgraph
-def subgraphResults(doc_name, graph, threshold):
+def subgraphResults(sdm_result):
+  process_start = time()
+  topic_number = sdm_result[0]
+  doc_name = sdm_result[1]
+  print("Process Start: " + doc_name)
   G = nx.Graph()
   top_results = {}
   for edge in list(nx.bfs_edges(graph, source=doc_name)):
@@ -37,45 +47,46 @@ def subgraphResults(doc_name, graph, threshold):
     if doc_count == 0:
       break
     top_results[doc] = score
+    if doc in scores[topic_number] and score < scores[topic_number][doc]:
+      continue
+    scores[topic_number][doc] = score
     doc_count = doc_count - 1
-  return top_results
-
-sdm_results = 'EB02-WordEmbeddings\\result_TF_IDF.txt'
-G = getGraph('EB02-WordEmbeddings\clueweb09PoolFilesTest.json')
-threshold = get_threshold(G)
+  print("Process End: " + doc_name + ". Took " + str(time() - process_start) + " s")
 
 # Get results
 with open(sdm_results) as f:
     rankings = [line.rstrip('\n') for line in f]
+
+sdm_docs = {}
 output = []
-scores = {}
-# Get query i sdm results
-prev_topic_number = 0
+
 sdm_count = 1
+# Map topic number to list of docs
 for rank in rankings:
   split = rank.split()
   topic_number = split[0]
-  if prev_topic_number == 0:
-    prev_topic_number = topic_number
   doc_name = split[2]
-  if prev_topic_number != topic_number or sdm_count == len(rankings):
-    doc_count = 1
-    for doc, score in sorted(scores.items(), key=lambda item: item[1], reverse = True):
-      if doc_count > 20:
-        break
-      output.append(prev_topic_number + " Q0 " + doc + " " + str(doc_count) + " " + str(normalize(score, scores.values()))+ " STANDARD")
-      doc_count += 1
-    scores = {}
-    prev_topic_number = topic_number
-  try:
-    subgraph = subgraphResults(doc_name, G, threshold)
-  except:
-    print("Document does not exist!")
-    continue
-  for doc, score in subgraph.items():
-    scores[doc] = score
-  sdm_count += 1
+  if topic_number not in sdm_docs:
+    sdm_docs[topic_number] = []
+  sdm_docs[topic_number].append((topic_number,doc_name))
 
-print(len(rankings))
+start_load = time()
+pool = ThreadPool(30)
+for topic_number in sdm_docs:
+  if topic_number not in scores:
+    scores[topic_number] = {}
+  subgraph = pool.map(subgraphResults, sdm_docs[topic_number])
+print("Subgraphs completed: " + str(time() - start_load) +  "s")
+
+for topic_number in scores:
+  cur_topic_subgraphs = scores[topic_number]
+  doc_count = 1
+  for doc, score in sorted(cur_topic_subgraphs.items(), key=lambda item: item[1], reverse = True):
+    if doc_count > 20:
+      break
+    output.append(topic_number + " Q0 " + doc + " " + str(doc_count) + " " + str(normalize(score, cur_topic_subgraphs.values()))+ " STANDARD")
+    doc_count += 1
+
 with open("results_file.test", "w") as outfile:
     outfile.write("\n".join(output))
+print ("Results took %.2f seconds to run." %(time() - start_project))
